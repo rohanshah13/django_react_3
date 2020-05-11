e5cb95bc080b43f48831b1bc041f9c10134694fc
Rohan Shah <rohanshah1306@gmail.com>
2020-05-11 23:29:16 +0530

    5th

--- chat_app/settings.py
+++ chat_app/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-#ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = [ 'guarded-bayou-10058.herokuapp.com']
 
 os.environ["DJANGO_ALLOW_ASYNC_UNSAFE"] = "true"
 # Application definition
